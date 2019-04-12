420c19b23632b6c845799500244b7dfe86ab1ed9
kumavis <aaron@kumavis.me>
2019-04-12 16:25:44 +0800

    lint - fix

--- ui/app/selectors/selectors.js
+++ ui/app/selectors/selectors.js
@@ -1,7 +1,6 @@
 import { NETWORK_TYPES } from '../helpers/constants/common'
 import { stripHexPrefix, addHexPrefix } from 'ethereumjs-util'
 
-
 const abi = require('human-standard-token-abi')
 import {
   transactionsSelector,
@@ -49,13 +48,10 @@ const selectors = {
   getNumberOfAccounts,
   getNumberOfTokens,
   isEthereumNetwork,
-<<<<<<< HEAD
   getMetaMetricState,
   getRpcPrefsForCurrentProvider,
   getKnownMethodData,
-=======
   getPermissionsDescriptions,
->>>>>>> 561cedb5a... ui - add permissions approval ui
 }
 
 module.exports = selectors
@@ -323,7 +319,6 @@ function getAdvancedInlineGasShown (state) {
   return Boolean(state.metamask.featureFlags.advancedInlineGas)
 }
 
-<<<<<<< HEAD
 function getMetaMetricState (state) {
   return {
     network: getCurrentNetworkId(state),
@@ -353,8 +348,7 @@ function getKnownMethodData (state, data) {
 
   return knownMethodData && knownMethodData[fourBytePrefix]
 }
-=======
+
 function getPermissionsDescriptions (state) {
   return state.metamask.permissionsDescriptions
 }
->>>>>>> 561cedb5a... ui - add permissions approval ui
