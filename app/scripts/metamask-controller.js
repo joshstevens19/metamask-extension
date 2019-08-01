7dd6a00fd5543392873ffd3b5bea1abebfc12fe5
Dan Miller <danjm.com@gmail.com>
2019-08-01 12:10:53 -0230

    Correct capilalization on 3Box

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -526,7 +526,7 @@ module.exports = class MetamaskController extends EventEmitter {
       // onboarding controller
       setSeedPhraseBackedUp: nodeify(onboardingController.setSeedPhraseBackedUp, onboardingController),
 
-      // 3box syncing
+      // 3Box syncing
       setThreeBoxSyncing: nodeify(threeBoxController.setThreeBoxSyncing, threeBoxController),
     }
   }
